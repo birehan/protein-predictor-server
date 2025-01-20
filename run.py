0d7deae81adbdb34a138a4cf2c4218515e6a21c7
birehan <birehananteneh4@gmail.com>
2025-01-20 11:44:20 +0300

    fix

--- run.py
+++ run.py
@@ -1,17 +1,9 @@
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=8080)
-    
-#     # app.run(debug=True)
-
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    
+    # app.run(debug=True)
